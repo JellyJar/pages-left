bb92960b7539ea5c321f453df75da93a6cb6fec3
dseptem <d.septem@gmail.com>
2016-08-31 00:40:06 -0300

    small fixes
    saved some lines of code, added round to nearest 100th

--- pagesLeft.py
+++ pagesLeft.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3
+pagesRead = int(input('How many pages have you read?\n'))  # you can convert to input to int
+daysLeft = int(input('How many days do you have left?\n'))
 
-pagesRead = input('How many pages have you read?\n')
-daysLeft = input('How many days do you have left?\n')
-pagesRead1 = int(pagesRead)
-daysLeft1 = int(daysLeft)
-totalPages = #Insert total number of pages here
-
-d = str((totalPages - pagesRead1) / daysLeft1)
+d = str((totalPages - pagesRead) / daysLeft)
 pagesLeft = totalPages - int(pagesRead)
-#Quickfix - need to round float to nearest hundredth.
-    #This current version rounds down to nearest integer.
-percentage = int(100*(pagesRead1 / totalPages)) 
+percentage = round(100*(pagesRead / totalPages), 3)  # rounds float to nearest hundredth
 
 if d.endswith('.0'):
-	pagesPerDay = int((totalPages - pagesRead1) / daysLeft1)
+	pagesPerDay = int((totalPages - pagesRead) / daysLeft)
 else:
-	pagesPerDay = int((totalPages - pagesRead1) / daysLeft1 + 1)
+	pagesPerDay = int((totalPages - pagesRead) / daysLeft + 1)
 
 print('''\nYou have %s days left to read %s more pages:
 You are %s%% done.
